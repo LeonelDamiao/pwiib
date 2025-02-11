8466c7e921812427db7b0232b6b2831c86bad639
Lab05_etec <Lab05_etec@etec>
2025-02-11 20:45:02 -0300

    página de produtos

---
+++ produtos .php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    < a href="index.php">página inicial</a>
+    < a href="contato.php"> Entre em contato </a>
+    <h1>Lista de Produtos</h1>
+    <ol>
+        <?php
+        for($i=0; $i<3; $i==)
+        {
+            echo "<li>Item $i</li>";
+        }
+        ?>
+        </ol>
+
+</body>
+</html>
